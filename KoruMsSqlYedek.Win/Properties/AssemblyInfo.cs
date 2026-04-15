84374122ca90e7033eb8ae37ab4224e6fb1d25d1
Hüseyin Küçük <hzkucuk@gmail.com>
2026-04-15 05:19:38 +0300

    fix: sessiz güncelleme scheduled task kaldırıldı, basit runas+/VERYSILENT yaklaşımına dönüldü

--- KoruMsSqlYedek.Win/Properties/AssemblyInfo.cs
+++ KoruMsSqlYedek.Win/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Yapı Numarası
 //      Düzeltme
 //
-[assembly: AssemblyVersion("0.99.66.0")]
-[assembly: AssemblyFileVersion("0.99.66.0")]
+[assembly: AssemblyVersion("0.99.67.0")]
+[assembly: AssemblyFileVersion("0.99.67.0")]
 
 [assembly: InternalsVisibleTo("KoruMsSqlYedek.Tests")]
